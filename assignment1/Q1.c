e6b55c017268be91f563fcb7e8b6f46e8581a171
Nathan Sosnovske <nathan@sosnovske.com>
2015-03-29 17:00:35 -0700

    Finished assignment 1

--- assignment1/Q1.c
+++ assignment1/Q1.c
@@ -7,6 +7,9 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include <time.h>
+
+const int NUM_STUDENTS = 10;
 
 struct student{
 	int id;
@@ -15,13 +18,24 @@ struct student{
 
 struct student* allocate(){
      /*Allocate memory for ten students*/
-     
+     struct student* students = malloc(sizeof(struct student) * NUM_STUDENTS);
      /*return the pointer*/
+    return students;
+}
+
+int randInt(int minInclusive, int maxInclusive)
+{
+    int range = (maxInclusive - minInclusive + 1);
+    return ((rand() % range) + minInclusive);
 }
 
 void generate(struct student* students){
      /*Generate random ID and scores for ten students, ID being between 1 and 10, scores between 0 and 100*/
-     
+     for(int i = 0; i < NUM_STUDENTS; i++)
+     {
+         students[i].id = randInt(1, 10);
+         students[i].score = randInt(0, 100);
+     }
 }
 
 void output(struct student* students){
@@ -31,29 +45,64 @@ void output(struct student* students){
               ID3 score3
               ...
               ID10 score10*/
+
+    for(int i = 0; i < NUM_STUDENTS; i++)
+    {
+        printf("%d %d\n", students[i].id, students[i].score);
+    }
+
+    printf("\n");
 }
 
 void summary(struct student* students){
      /*Compute and print the minimum, maximum and average scores of the ten students*/
+    int min = -1;
+    int max = -1;
+    int total = 0;
+    int average;
+
+    for(int i = 0; i < NUM_STUDENTS; i++)
+    {
+        if(min > students[i].score || min == -1)
+            min = students[i].score;
+        if(max < students[i].score)
+            max = students[i].score;
+
+        total += students[i].score;
+    }
+
+    average = total / NUM_STUDENTS;
+
+    printf("Min: %d\n", min);
+    printf("Max: %d\n", max);
+    printf("Average: %d\n", average);
+
      
 }
 
 void deallocate(struct student* stud){
      /*Deallocate memory from stud*/
+     free(stud);
 }
 
 int main(){
+    srand(time(NULL));
     struct student* stud = NULL;
     
     /*call allocate*/
+    stud = allocate();
     
     /*call generate*/
-    
+    generate(stud);
+
     /*call output*/
+    output(stud);
     
     /*call summary*/
+    summary(stud);
     
     /*call deallocate*/
+    deallocate(stud);
 
     return 0;
 }
--- assignment1/Q5.c
+++ assignment1/Q5.c
@@ -6,27 +6,39 @@
  
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-/*converts ch to upper case, assuming it is in lower case currently*/
-char toUpperCase(char ch){
-     return ch-'a'+'A';
-}
-
-/*converts ch to lower case, assuming it is in upper case currently*/
-char toLowerCase(char ch){
-     return ch-'A'+'a';
-}
+const int MAX_LETTERS = 1000;
 
 void sticky(char* word){
      /*Convert to sticky caps*/
+     for(int i = 0; i < MAX_LETTERS; i++)
+     {
+         if(word[i] == '\0')
+             return;
+
+         if(i % 2)
+             word[i] = tolower(word[i]);
+         else
+             word[i] = toupper(word[i]);
+     }
+
 }
 
 int main(){
     /*Read word from the keyboard using scanf*/
-    
+
+    /* I'm using scanf because the spec calls for it... but it seems like a bad choice for user input
+     * given that a buffer overflow is possible. Something like getchar() in a while loop or fgets might be
+     * better?
+     */
+
+    char buffer[MAX_LETTERS];
+    scanf("%s", buffer);
     /*Call sticky*/
-    
+    sticky(buffer);
     /*Print the new word*/
+    printf("%s\n", buffer);
     
     return 0;
 }
