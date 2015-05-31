ec1ce7de340e29b24aa833e5e6d901927819c5da
Nathan Sosnovske <nathan@sosnovske.com>
2015-05-31 16:09:31 -0700

    Revert "Only remove if value is 1"
    This reverts commit 0214586723b2b5a36da41b5766d479a0f539fea6.

--- assignment6/hashMap.c
+++ assignment6/hashMap.c
@@ -276,14 +276,9 @@ void removeKey (struct hashMap * ht, KeyType k)
 
 	if (strcmp(currentLink->key, k))
 	{
-		if (currentLink->value > 1)
-			currentLink->value--;
-		else
-		{
-			free(currentLink->key);
-			ht->table[hash] = nextLink;
-			free(currentLink);
-		}
+		free(currentLink->key);
+		ht->table[hash] = nextLink;
+		free(currentLink);
 		return;
 	}
 
@@ -291,14 +286,9 @@ void removeKey (struct hashMap * ht, KeyType k)
 	{
 		if (strcmp(nextLink->key, k) == 0)
 		{
-			if (nextLink->value > 1)
-				nextLink->value--;
-			else
-			{
-				free(nextLink->key);
-				currentLink->next = nextLink->next;
-				free(nextLink);
-			}
+			free(nextLink->key);
+			currentLink->next = nextLink->next;
+			free(nextLink);
 		}
 		currentLink = currentLink->next;
 		nextLink = nextLink->next;
